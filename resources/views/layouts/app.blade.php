a8b289a71683e104bf44e120eaac8c85450adfc8
Pavel Mikhaylov <mikhailov.sztu@gmail.com>
2021-01-13 21:20:13 +0300

    fix html spaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,57 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Page Analyzer</title>
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="csrf-param" content="_token" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/app.js') }}"></script>
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page Analyzer</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param" content="_token" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
-              <a class="navbar-brand" href="/">Page Analyzer</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('domains.index') }}">Domains</a>
-                  </li>
-                </ul>
-              </div>
+                <a class="navbar-brand" href="/">Page Analyzer</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page"
+                                href="{{ route('domains.index') }}">Domains</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-          </nav>
-          <main class="flex-grow-1">
-                @include('flash::message')
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
-                    @yield('content')
-          </main>
-    </body>
+        </nav>
+    </header>
+    <main class="flex-grow-1">
+        @include('flash::message')
+        @if($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @yield('content')
+    </main>
+    <footer class="border-top py-3 mt-5">
+        <div class="container-lg">
+            <div class="text-center">
+                created by
+                <a href="https://github.com/badta5te" target="_blank">Pavel Mikhaylov</a>
+            </div>
+        </div>
+    </footer>
+</body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="flex-center position-ref full-height">
-        <div class="container-lg">
-            <div class="row">
-                <div class="col-12 col-md-10 col-lg-8 mx-auto">
-                    <h1 class="display-3">Page Analyzer</h1>
-                    <p class="lead">Check web pages for free</p>
-                    <form action="{{ route('domains.store') }}" method="post" class="d-flex justify-content-center">
-                        @csrf
-                        <input type="text" name="domain[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com" required>
-                        <button type="submit" class="btn btn-lg btn-primary ml-3 px-5 text-uppercase">Check</button>
-                    </form>
-                </div>
+<div class="flex-center position-ref full-height">
+    <div class="container-lg">
+        <div class="row">
+            <div class="col-12 col-md-10 col-lg-8 mx-auto">
+                <h1 class="display-3">Page Analyzer</h1>
+                <p class="lead">Check web pages for free</p>
+                <form action="{{ route('domains.store') }}" method="post"
+                    class="d-flex justify-content-center">
+                    @csrf
+                    <input type="text" name="domain[name]" value="" class="form-control form-control-lg"
+                        placeholder="https://www.example.com" required>
+                    <button type="submit" class="btn btn-lg btn-primary ml-1 px-5 text-uppercase">Check</button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection
